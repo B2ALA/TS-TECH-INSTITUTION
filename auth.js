4a8faf2e8672399b800c49cb1265d990a1f6f251
B2ALA <24balat@gmail.com>
2026-06-20 19:43:12 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,20 @@
 async function signup() {
 
+  alert("Signup function started");
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const { data, error } = await supabaseClient.auth.signUp({
-    email,
-    password
-  });
+  console.log(email, password);
+
+  const { data, error } =
+    await supabaseClient.auth.signUp({
+      email,
+      password
+    });
+
+  console.log(data);
+  console.log(error);
 
   if (error) {
     alert(error.message);
