a8f8a1035bc187d05052734a0ceeb2b8b2854280
B2ALA <24balat@gmail.com>
2026-06-26 12:37:04 +0530

    Delete middleware directory

--- middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-    if (!req.user || req.user.role !== 'admin') {
-        return res.status(403).json({ error: "Access Denied. Root privilege or administrative key validation required." });
-    }
-    next();
-};
-
-module.exports = adminMiddleware;
